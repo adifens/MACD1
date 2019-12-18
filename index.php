899a2d6a186208c80c7cc0f038fed3987b136d95
adifens <dickyareda@gmail.com>
2019-12-18 18:39:33 +0700

    pas

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
   
     // PHP Data Objects(PDO) Sample Code:
     try {
-        $conn = new PDO("sqlsrv:server = tcp:databasekuserver.database.windows.net,1433; Database = databaseku", "adifens", "");
+        $conn = new PDO("sqlsrv:server = tcp:databasekuserver.database.windows.net,1433; Database = databaseku", "adifens", "Efendy17");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $e) {
@@ -44,7 +44,7 @@
     }
     
     // SQL Server Extension Sample Code:
-    $connectionInfo = array("UID" => "adifens", "pwd" => "", "Database" => "databaseku", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $connectionInfo = array("UID" => "adifens", "pwd" => "Efendy17", "Database" => "databaseku", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
     $serverName = "tcp:databasekuserver.database.windows.net,1433";
     $conn = sqlsrv_connect($serverName, $connectionInfo);
 
